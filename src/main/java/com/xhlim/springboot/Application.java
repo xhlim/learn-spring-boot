62942a9cea16d4e735c463e505fe7a8ebda44316
xhlim <xhlim@outlook.com>
2018-03-16 17:14:24 +0800

    创建应用

---
+++ src/main/java/com/xhlim/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.xhlim.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xhlim@outlook.com
+ * @create 2018-03-16
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
